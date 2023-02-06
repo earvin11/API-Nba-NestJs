1b46d8698fd795d27a4c7c5b2f16b194d879c5e3
Earvin Gomez <earvinsantiago.11@gmail.com>
2023-02-06 10:09:59 -0400

    JoiValidation

--- src/config/joi.validation.ts
+++ src/config/joi.validation.ts
@@ -1,7 +1,7 @@
 import * as Joi from 'joi';
 
 export const JoiValidationSchema = Joi.object({
-    PORT: Joi.required(),
-    MONGODB: Joi.number().default(3005),
+    MONGODB: Joi.required(),
+    PORT: Joi.number().default(3005),
     DEFAULT_LIMIT: Joi.number().default(6),
 });
